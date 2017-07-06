59d540e430c080a6d8205ce2ac7ca8a2304322e2
shaunlazaro <shaunlazaro@gmail.com>
2017-07-06 17:12:58 -0400

    Initial Commit (Real)

---
+++ Summer Side Project 2017/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float jumpHeight;
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown ("Jump") )
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+        if(Input.GetAxis("Horizontal") < -0.2 && GetComponent<Rigidbody2D>().velocity.x > -moveSpeed)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetAxis("Horizontal") > 0.2 && GetComponent<Rigidbody2D>().velocity.x < moveSpeed)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
