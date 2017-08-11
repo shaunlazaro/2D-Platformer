9ace256af5f5c0fa4ab12ac7331dc60a9e671237
shaunlazaro <shaunlazaro@gmail.com>
2017-08-11 15:02:36 -0400

    Sprint 4-C2: Enemy Spawn Point
    Created "Enemy Spawn Point" script.
    With this, Sprint 4 is complete.

---
+++ Summer Side Project 2017/Assets/Scripts/EnemySpawnPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour {
+
+    public GameObject enemyToSpawn;
+    public int localScaleX;
+
+	// Use this for initialization
+	void Start () {
+        SpawnEnemy(localScaleX);
+	}
+
+    public void SpawnEnemy(int localScalex = 1)
+    {
+        GameObject enemy = Instantiate(enemyToSpawn, this.gameObject.transform);
+        enemy.transform.localScale = new Vector3(localScalex, 1, 1);
+    }
+}
